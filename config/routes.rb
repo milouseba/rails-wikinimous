03ddddefbe9ddaf5ebff11b52f63754450e22a00
sebastien milou <milouseba@gmail.com>
2017-08-08 15:40:43 +0200

    ajout de routes + index list

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/index'
 
-  get 'articles/show'
 
-  get 'articles/new'
+  root to: 'articles#index'
 
-  get 'articles/create'
-
-  get 'articles/update'
-
-  get 'articles/edit'
-
-  get 'articles/destroy'
-
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

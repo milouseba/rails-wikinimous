7d2ef4038f0da675d7fdcedf078874d50fba6c51
sebastien milou <milouseba@gmail.com>
2017-08-09 11:28:25 +0200

    exo veille

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,32 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id].to_i)
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(title: params[:article][:title], content: params[:article][:content])
+    @article.save
+    redirect_to article_path(@article[:id])
   end
 
   def update
+    @article = Article.find(params[:id].to_i)
+    @article.update(title: params[:article][:title], content: params[:article][:content])
+    redirect_to article_path(@article[:id])
   end
 
   def edit
+    @article = Article.find(params[:id].to_i)
   end
 
   def destroy
+    @article = Article.find(params[:id].to_i)
+    @article.delete
+    redirect_to articles_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :tasks
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
